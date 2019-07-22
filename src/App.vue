6de68ee525d282d904c33dd8532935b80054bec6
Chen Dar-Yen <chendaryen@outlook.com>
2019-07-22 16:14:30 +0800

    first version

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import './assets/reset';
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  user-select: none;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/modal.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="modal">
+    <div class="container">
+      <div class="alert">
+        <img src="../assets/images/alert.svg" width="135px">
+      </div>
+
+      <p class="message"
+      v-for="(message, index) in messages" :key="index">
+        {{ message }}
+      </p>
+
+      <div class="btn-container">
+        <div>
+          <button class="btn"
+          @click="act">{{ btnMsg }}</button>
+        </div>
+        <div>
+          <button class="btn btn-sub"
+          @click="hide">Back to current game</button>
+        </div>   
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue, Prop, Emit } from 'vue-property-decorator';
+
+@Component({})
+
+export default class Modal extends Vue {
+  @Prop(Array) private messages;
+  @Prop(String) private btnMsg;
+
+  @Emit() public act () {
+    return;
+  }
+  @Emit() public hide () {
+    return;
+  }
+}
+</script>
+
+<style lang='scss' scoped >
+@import '../assets/all';
+
+.modal {
+  width: 100%;
+  height: 100%;
+  font-weight: 900;
+  background-color: rgba(1, 1, 1, .7);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+}
+.container {
+  padding: 50px 115px;
+  background-color: #ffffff;
+  border-radius: 20px;
+  text-align: center;
+}
+.alert {
+  margin-bottom: 10px;
+}
+.message {
+  line-height: 1.5;
+}
+.btn-container {
+  margin-top: 30px;
+}
+</style>
